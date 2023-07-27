7a893da86c2dfc2bca6bcad19eedb16c1b9425e2
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-28 00:05:11 +0300

    javascript

--- script.js
+++ script.js
@@ -1,42 +1,101 @@
-// your-script.js
+function redirectToSpotifyAuth() {
+  const clientId = '473e1292e9714be2b9defd20feebd4eb';
+  const redirectUri = encodeURIComponent('https://spotify-to-youtube.vercel.app/');
+  const scopes = encodeURIComponent('user-library-read');
 
-const clientId = 'YOUR_SPOTIFY_CLIENT_ID';
-const redirectUri = 'YOUR_REDIRECT_URI'; // This should be a URL on your website to handle the authorization callback
+  window.location = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+}
 
-// Check if the user is already logged in and authorized
-function checkAuthorization() {
-  const accessToken = localStorage.getItem('spotifyAccessToken');
-  if (accessToken) {
-    // User is authorized, show the authorized section
-    document.getElementById('loginSection').style.display = 'none';
-    document.getElementById('authorizedSection').style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+  const logButton = document.getElementById('loginButton');
+  logButton.addEventListener('click', redirectToSpotifyAuth);
+});
 
-    // Get user's information using the access token (You can implement this)
-    getUserInfo(accessToken);
-  } else {
-    // User is not authorized, show the login section
-    document.getElementById('loginSection').style.display = 'block';
-    document.getElementById('authorizedSection').style.display = 'none';
+
+async function getAccessToken(code) {
+  const clientId = '473e1292e9714be2b9defd20feebd4eb';
+  const clientSecret = 'b58bd34301a9493dbba1a1e36fcd4fe3';
+  const redirectUri = 'https://spotify-to-youtube.vercel.app/';
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+      },
+      body: new URLSearchParams({
+          'grant_type': 'authorization_code',
+          'code': code,
+          'redirect_uri': redirectUri
+      })
+  });
+
+  const data = await response.json();
+
+  return data.access_token;
+}
+
+async function getLikedSongs(accessToken) {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+      headers: {
+          'Authorization': 'Bearer ' + accessToken
+      }
+  });
+
+  const data = await response.json();
+
+  return data.items.map(item => item.track);
+}
+
+async function searchYoutube(songName, artistName) {
+  const apiKey = 'AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8';
+  const query = encodeURIComponent(songName + ' ' + artistName);
+  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${apiKey}`);
+
+  const data = await response.json();
+
+  return data.items[0].id.videoId;
+}
+
+
+function displaySongs(songs, youtubeLinks) {
+  const list = document.getElementById('song-list');
+
+  for (let i = 0; i < songs.length; i++) {
+      const song = songs[i];
+      const listItem = document.createElement('li');
+
+      const songInfo = document.createElement('p');
+      songInfo.textContent = song.name + ' by ' + song.artists[0].name;
+      listItem.appendChild(songInfo);
+
+      const youtubeEmbed = document.createElement('iframe');
+      youtubeEmbed.width = "560";
+      youtubeEmbed.height = "315";
+      youtubeEmbed.src = 'https://www.youtube.com/embed/' + youtubeLinks[i];
+      youtubeEmbed.title = "YouTube video player";
+      youtubeEmbed.frameborder = "0";
+      youtubeEmbed.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+      youtubeEmbed.allowFullscreen = true;
+      listItem.appendChild(youtubeEmbed);
+
+      list.appendChild(listItem);
   }
 }
 
-// Handle the login button click event
-document.getElementById('loginButton').addEventListener('click', () => {
-  // Redirect the user to Spotify's login page
-  window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=user-read-private`;
-});
+async function handleAuthResponse() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
 
-// After successful authorization, Spotify will redirect back to your website with an authorization code
-// You need to handle this callback on your redirectUri page and exchange the code for an access token
-// The access token should then be stored securely (e.g., using localStorage) for future use.
+  if (code) {
+      const accessToken = await getAccessToken(code);
+      const songs = await getLikedSongs(accessToken);
+      const youtubeLinks = await Promise.all(songs.map(song => searchYoutube('AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8', song.name, song.artists[0].name)));
+      displaySongs(songs, youtubeLinks);
 
-// Implement the function to get user's information using the access token
-function getUserInfo(accessToken) {
-  // Use the access token to make requests to the Spotify Web API and get user data
-  // Implement your API calls here to fetch user data, scrape songs, etc.
-  // Remember to handle API rate limits and error handling
-  // Display user data and other functionalities in the authorized section
+      document.getElementById('loginSection').style.display = 'none';
+      document.getElementById('authorizedSection').style.display = 'block';
+  }
 }
 
-// When the page loads, check if the user is already authorized
-checkAuthorization();
+window.onload = handleAuthResponse;
