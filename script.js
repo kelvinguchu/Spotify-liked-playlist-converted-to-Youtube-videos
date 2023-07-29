0bcd488ff9e507bab1fd6ebcdf6569b40a4dc306
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-29 12:54:47 +0300

    javascript

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-function redirectToSpotifyAuth() {
-  const clientId = '473e1292e9714be2b9defd20feebd4eb';
-  const redirectUri = encodeURIComponent('https://spotify-to-youtube.vercel.app/');
-  const scopes = encodeURIComponent('user-library-read');
+const clientId = '473e1292e9714be2b9defd20feebd4eb';
+const clientSecret = 'b58bd34301a9493dbba1a1e36fcd4fe3';
+const redirectUri = 'https://youtubify-gamma.vercel.app/';
+const apiKey = 'AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8';
 
-  window.location = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+function redirectToSpotifyAuth() {
+  const scopes = encodeURIComponent('user-library-read');
+  window.location = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const logButton = document.getElementById('loginButton');
-  logButton.addEventListener('click', redirectToSpotifyAuth);
-});
-
-
 async function getAccessToken(code) {
-  const clientId = '473e1292e9714be2b9defd20feebd4eb';
-  const clientSecret = 'b58bd34301a9493dbba1a1e36fcd4fe3';
-  const redirectUri = 'https://spotify-to-youtube.vercel.app/';
-
   const response = await fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
       headers: {
@@ -29,58 +21,45 @@ async function getAccessToken(code) {
           'redirect_uri': redirectUri
       })
   });
-
   const data = await response.json();
-
   return data.access_token;
 }
 
 async function getLikedSongs(accessToken) {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
-      headers: {
-          'Authorization': 'Bearer ' + accessToken
-      }
+      headers: { 'Authorization': 'Bearer ' + accessToken }
   });
-
   const data = await response.json();
-
   return data.items.map(item => item.track);
 }
 
 async function searchYoutube(songName, artistName) {
-  const apiKey = 'AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8';
   const query = encodeURIComponent(songName + ' ' + artistName);
   const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${apiKey}`);
-
   const data = await response.json();
-
   return data.items[0].id.videoId;
 }
 
-
 function displaySongs(songs, youtubeLinks) {
   const list = document.getElementById('song-list');
+  songs.forEach((song, i) => {
+    const listItem = document.createElement('li');
+    const songInfo = document.createElement('p');
+    songInfo.textContent = `${song.name} by ${song.artists[0].name}`;
+    listItem.appendChild(songInfo);
 
-  for (let i = 0; i < songs.length; i++) {
-      const song = songs[i];
-      const listItem = document.createElement('li');
+    const youtubeEmbed = document.createElement('iframe');
+    youtubeEmbed.width = "560";
+    youtubeEmbed.height = "315";
+    youtubeEmbed.src = `https://www.youtube.com/embed/${youtubeLinks[i]}`;
+    youtubeEmbed.title = "YouTube video player";
+    youtubeEmbed.frameborder = "0";
+    youtubeEmbed.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    youtubeEmbed.allowFullscreen = true;
+    listItem.appendChild(youtubeEmbed);
 
-      const songInfo = document.createElement('p');
-      songInfo.textContent = song.name + ' by ' + song.artists[0].name;
-      listItem.appendChild(songInfo);
-
-      const youtubeEmbed = document.createElement('iframe');
-      youtubeEmbed.width = "560";
-      youtubeEmbed.height = "315";
-      youtubeEmbed.src = 'https://www.youtube.com/embed/' + youtubeLinks[i];
-      youtubeEmbed.title = "YouTube video player";
-      youtubeEmbed.frameborder = "0";
-      youtubeEmbed.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-      youtubeEmbed.allowFullscreen = true;
-      listItem.appendChild(youtubeEmbed);
-
-      list.appendChild(listItem);
-  }
+    list.appendChild(listItem);
+  });
 }
 
 async function handleAuthResponse() {
@@ -98,4 +77,8 @@ async function handleAuthResponse() {
   }
 }
 
-window.onload = handleAuthResponse;
+document.addEventListener('DOMContentLoaded', function() {
+  const logButton = document.getElementById('loginButton');
+  logButton.addEventListener('click', redirectToSpotifyAuth);
+  window.onload = handleAuthResponse;
+});
