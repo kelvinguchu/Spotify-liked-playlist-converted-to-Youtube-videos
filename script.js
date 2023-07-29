0011fdc076256a76a8497b3c3e57ce3dbf84eba2
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-29 17:02:51 +0300

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const clientId = '473e1292e9714be2b9defd20feebd4eb';
 const clientSecret = 'b58bd34301a9493dbba1a1e36fcd4fe3';
-const redirectUri = 'https://youtubify-gamma.vercel.app/';
+const redirectUri = 'http://127.0.0.1:5500/index.html';
 const apiKey = 'AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8';
 
 function redirectToSpotifyAuth() {
@@ -21,21 +21,34 @@ async function getAccessToken(code) {
           'redirect_uri': redirectUri
       })
   });
-  const data = await response.json();
-  return data.access_token;
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    const data = await response.json();
+    return data.access_token;
+  }
 }
 
-async function getLikedSongs(accessToken) {
-  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+async function getLikedSongs(accessToken, url = 'https://api.spotify.com/v1/me/tracks?limit=50') {
+  const response = await fetch(url, {
       headers: { 'Authorization': 'Bearer ' + accessToken }
   });
   const data = await response.json();
-  return data.items.map(item => item.track);
+  
+  let tracks = data.items.map(item => item.track);
+  
+  if (data.next) {
+      const nextTracks = await getLikedSongs(accessToken, data.next);
+      tracks = tracks.concat(nextTracks);
+  }
+  
+  return tracks;
 }
 
 async function searchYoutube(songName, artistName) {
   const query = encodeURIComponent(songName + ' ' + artistName);
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${apiKey}`);
+  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&videoEmbeddable=true&regionCode=US&type=video&q=${query}&key=${apiKey}`);
   const data = await response.json();
   return data.items[0].id.videoId;
 }
@@ -49,11 +62,12 @@ function displaySongs(songs, youtubeLinks) {
     listItem.appendChild(songInfo);
 
     const youtubeEmbed = document.createElement('iframe');
-    youtubeEmbed.width = "560";
-    youtubeEmbed.height = "315";
+    youtubeEmbed.width = "100%";  // Adjusts the width to be 100% of the parent's width
+    youtubeEmbed.height = "auto";  // Height is automatically adjusted to maintain the aspect ratio
     youtubeEmbed.src = `https://www.youtube.com/embed/${youtubeLinks[i]}`;
     youtubeEmbed.title = "YouTube video player";
     youtubeEmbed.frameborder = "0";
+    youtubeEmbed.style.borderRadius = "15px";  // Adds border-radius to the video embed
     youtubeEmbed.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
     youtubeEmbed.allowFullscreen = true;
     listItem.appendChild(youtubeEmbed);
@@ -67,6 +81,7 @@ async function handleAuthResponse() {
   const code = urlParams.get('code');
 
   if (code) {
+    try {
       const accessToken = await getAccessToken(code);
       const songs = await getLikedSongs(accessToken);
       const youtubeLinks = await Promise.all(songs.map(song => searchYoutube(song.name, song.artists[0].name))); 
@@ -74,6 +89,9 @@ async function handleAuthResponse() {
 
       document.getElementById('loginSection').style.display = 'none';
       document.getElementById('authorizedSection').style.display = 'block';
+    } catch (error) {
+      console.error(`Failed to get access token or liked songs: ${error}`);
+    }
   }
 }
 
