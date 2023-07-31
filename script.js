25eb463579ae1e11f3d325771725b70e0858d60d
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-31 21:05:31 +0300

    javascript

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+// Define the constants for the Spotify API
 const clientId = '473e1292e9714be2b9defd20feebd4eb';
 const clientSecret = 'b58bd34301a9493dbba1a1e36fcd4fe3';
 const redirectUri = 'http://127.0.0.1:5500/index.html';
+
+//The API key for YouTube
 const apiKey = 'AIzaSyDeby8kdPYzUQawOqFiNRp_UJ34Zmvaag8';
 
+// Function to redirect to Spotify Authentication page
 function redirectToSpotifyAuth() {
   const scopes = encodeURIComponent('user-library-read');
   window.location = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-}
+};
 
+// Function to retrieve access token from Spotify
 async function getAccessToken(code) {
   const response = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-      },
-      body: new URLSearchParams({
-          'grant_type': 'authorization_code',
-          'code': code,
-          'redirect_uri': redirectUri
-      })
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+    },
+    body: new URLSearchParams({
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirectUri
+    })
   });
 
   if (!response.ok) {
@@ -27,47 +32,60 @@ async function getAccessToken(code) {
   } else {
     const data = await response.json();
     return data.access_token;
-  }
-}
+  };
+};
 
+// Function to retrieve liked songs from Spotify
 async function getLikedSongs(accessToken, url = 'https://api.spotify.com/v1/me/tracks?limit=50') {
   const response = await fetch(url, {
       headers: { 'Authorization': 'Bearer ' + accessToken }
   });
   const data = await response.json();
-  
+
   let tracks = data.items.map(item => item.track);
   
   if (data.next) {
-      const nextTracks = await getLikedSongs(accessToken, data.next);
-      tracks = tracks.concat(nextTracks);
-  }
+    const nextTracks = await getLikedSongs(accessToken, data.next);
+    tracks = tracks.concat(nextTracks);
+  };
   
   return tracks;
-}
+};
 
+// Function to search YouTube and return first video ID, uses local storage to cache results
 async function searchYoutube(songName, artistName) {
+  const cacheKey = `${songName}-${artistName}`;
+  const cachedResult = localStorage.getItem(cacheKey);
+  
+  // If result is cached, return it
+  if (cachedResult) {
+    return cachedResult;
+  };
+
+  // Else, fetch data from YouTube API
   const query = encodeURIComponent(`${songName} ${artistName} official music video`);
   const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&videoEmbeddable=true&regionCode=US&type=video&q=${query}&key=${apiKey}`);
   const data = await response.json();
-  return data.items[0].id.videoId;
-}
 
+  const videoId = data.items[0].id.videoId;
+  // Cache the result in local storage
+  localStorage.setItem(cacheKey, videoId);
+  
+  return videoId;
+};
 
+// Function to display songs and their corresponding YouTube links on webpage
 function displaySongs(songs, youtubeLinks) {
   const list = document.getElementById('song-list');
   songs.forEach((song, i) => {
-    // Create card listItem with class 'card'
     const listItem = document.createElement('li');
     listItem.className = 'card';
     
-    // Create songInfo with class 'card_info'
     const songInfo = document.createElement('p');
     songInfo.className = 'card_info';
     songInfo.textContent = `${song.name} by ${song.artists[0].name}`;
     listItem.appendChild(songInfo);
 
-    // Create youtubeEmbed with parent 'card_video'
     const youtubeEmbedContainer = document.createElement('div');
     youtubeEmbedContainer.className = 'card_video';
     const youtubeEmbed = document.createElement('iframe');
@@ -84,9 +102,9 @@ function displaySongs(songs, youtubeLinks) {
     listItem.appendChild(youtubeEmbedContainer);
     list.appendChild(listItem);
   });
-}
+};
 
-
+// Function to retrieve user profile from Spotify
 async function getUserProfile(accessToken) {
   const response = await fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
@@ -97,9 +115,10 @@ async function getUserProfile(accessToken) {
   } else {
     const data = await response.json();
     return data;
-  }
-}
+  };
+};
 
+// Function to handle authentication response
 async function handleAuthResponse() {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
@@ -118,10 +137,11 @@ async function handleAuthResponse() {
       document.getElementById('authorizedSection').style.display = 'block';
     } catch (error) {
       console.error(`Failed to get access token, user profile, or liked songs: ${error}`);
-    }
-  }
-}
+    };
+  };
+};
 
+// Event listener to handle document ready event
 document.addEventListener('DOMContentLoaded', function() {
   const logButton = document.getElementById('loginButton');
   logButton.addEventListener('click', redirectToSpotifyAuth);
